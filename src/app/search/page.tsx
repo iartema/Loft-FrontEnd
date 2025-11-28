552553ebf54680745cf0386724f8ab24784f9a51
Artem Knysh 😈 <mrimfine45@gmail.com>
2025-11-28 16:36:16 +0100

    search page fix

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,8 +1,13 @@
 "use client"
 
+import { Suspense } from "react";
 // this is change
-import SearchView from '../components/organisms/SearchView';
+import SearchView from "../components/organisms/SearchView";
 
 export default function SearchPage() {
-  return <SearchView />;
+  return (
+    <Suspense fallback={<div className="p-8 text-white/70">Loading search...</div>}>
+      <SearchView />
+    </Suspense>
+  );
 }
